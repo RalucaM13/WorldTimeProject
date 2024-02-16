9f533f7bca68a6a38e922713b9b09ae295a71dec
RalucaM13 <157896258+RalucaM13@users.noreply.github.com>
2024-02-16 12:19:27 +0000

    Added date and time with moment and javascript

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,1 +1,11 @@
+function updateMelbTime() {
+  let melbElement = document.querySelector("#melb");
+  let melbDateElement = melbElement.querySelector(".date");
+  let melbTimeElement = melbElement.querySelector(".time");
+  let melbTime = moment().tz("Australia/Melbourne");
+  melbDateElement.innerHTML = melbTime.format("MMMM Do YYYY");
+  melbTimeElement.innerHTML = melbTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
+updateMelbTime();
+setInterval(updateMelbTime, 1000);
