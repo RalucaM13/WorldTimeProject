e940c318f0cadf176f45fa23ee3055914974cabe
RalucaM13 <157896258+RalucaM13@users.noreply.github.com>
2024-02-16 15:20:09 +0000

    Update functions.js

--- javascript/functions.js
+++ javascript/functions.js
@@ -49,7 +49,7 @@ setInterval(updateBuchTime, 1000);
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess;
+    cityTimeZone = moment.tz.guess();
   }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
